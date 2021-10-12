a7b91cce8c8424261a862e81f7757d9736e23cb9
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-10-12 09:24:43 +0600

    lowest product section on home page

--- src/components/ui/ProductsListCarousel.vue
+++ src/components/ui/ProductsListCarousel.vue
@@ -1,200 +1,34 @@
 <template>
-    <section class="latest-product spad">
+  <section class="latest-product spad">
     <div class="container">
+      <h4>Lowest Price Products</h4>
       <div class="row">
-        <div class="col-lg-4 col-md-6">
-          <div class="latest-product__text">
-            <h4>Latest Products</h4>
-            <div class="latest-product__slider owl-carousel">
-              <div class="latest-prdouct__slider__item">
-                <a href="#" class="latest-product__item">
-                  <div class="latest-product__item__pic">
-                    <img src="assets/img/latest-product/lp-1.jpg" alt="" />
-                  </div>
-                  <div class="latest-product__item__text">
-                    <h6>Crab Pool Security</h6>
-                    <span>$30.00</span>
-                  </div>
-                </a>
-                <a href="#" class="latest-product__item">
-                  <div class="latest-product__item__pic">
-                    <img src="assets/img/latest-product/lp-2.jpg" alt="" />
-                  </div>
-                  <div class="latest-product__item__text">
-                    <h6>Crab Pool Security</h6>
-                    <span>$30.00</span>
-                  </div>
-                </a>
-                <a href="#" class="latest-product__item">
-                  <div class="latest-product__item__pic">
-                    <img src="assets/img/latest-product/lp-3.jpg" alt="" />
-                  </div>
-                  <div class="latest-product__item__text">
-                    <h6>Crab Pool Security</h6>
-                    <span>$30.00</span>
-                  </div>
-                </a>
+        <div class="products__section">
+          <div
+            v-for="product in lowestProducts"
+            :key="product._id"
+            class="col-md-4 latest-prdouct__slider__item"
+          >
+            <router-link
+              :to="`/products/${product._id}`"
+              class="lowest__product__item"
+            >
+              <div class="lowest__product__item__pic">
+                <img :src="product.image" :alt="product.title" />
               </div>
-              <div class="latest-prdouct__slider__item">
-                <a href="#" class="latest-product__item">
-                  <div class="latest-product__item__pic">
-                    <img src="assets/img/latest-product/lp-1.jpg" alt="" />
-                  </div>
-                  <div class="latest-product__item__text">
-                    <h6>Crab Pool Security</h6>
-                    <span>$30.00</span>
-                  </div>
-                </a>
-                <a href="#" class="latest-product__item">
-                  <div class="latest-product__item__pic">
-                    <img src="assets/img/latest-product/lp-2.jpg" alt="" />
-                  </div>
-                  <div class="latest-product__item__text">
-                    <h6>Crab Pool Security</h6>
-                    <span>$30.00</span>
-                  </div>
-                </a>
-                <a href="#" class="latest-product__item">
-                  <div class="latest-product__item__pic">
-                    <img src="assets/img/latest-product/lp-3.jpg" alt="" />
-                  </div>
-                  <div class="latest-product__item__text">
-                    <h6>Crab Pool Security</h6>
-                    <span>$30.00</span>
-                  </div>
-                </a>
+              <div class="lowest__product__item__text">
+                <h6>{{ product.title.slice(0, 25) }}</h6>
+                <span>${{ product.price.toFixed(2) }}</span>
+                <p>
+                  <router-link
+                    :to="`/category/${product.category}`"
+                    class="lowest__product__item__text"
+                  >
+                    {{ product.category }}
+                  </router-link>
+                </p>
               </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 col-md-6">
-          <div class="latest-product__text">
-            <h4>Top Rated Products</h4>
-            <div class="latest-product__slider owl-carousel">
-              <div class="latest-prdouct__slider__item">
-                <a href="#" class="latest-product__item">
-                  <div class="latest-product__item__pic">
-                    <img src="assets/img/latest-product/lp-1.jpg" alt="" />
-                  </div>
-                  <div class="latest-product__item__text">
-                    <h6>Crab Pool Security</h6>
-                    <span>$30.00</span>
-                  </div>
-                </a>
-                <a href="#" class="latest-product__item">
-                  <div class="latest-product__item__pic">
-                    <img src="assets/img/latest-product/lp-2.jpg" alt="" />
-                  </div>
-                  <div class="latest-product__item__text">
-                    <h6>Crab Pool Security</h6>
-                    <span>$30.00</span>
-                  </div>
-                </a>
-                <a href="#" class="latest-product__item">
-                  <div class="latest-product__item__pic">
-                    <img src="assets/img/latest-product/lp-3.jpg" alt="" />
-                  </div>
-                  <div class="latest-product__item__text">
-                    <h6>Crab Pool Security</h6>
-                    <span>$30.00</span>
-                  </div>
-                </a>
-              </div>
-              <div class="latest-prdouct__slider__item">
-                <a href="#" class="latest-product__item">
-                  <div class="latest-product__item__pic">
-                    <img src="assets/img/latest-product/lp-1.jpg" alt="" />
-                  </div>
-                  <div class="latest-product__item__text">
-                    <h6>Crab Pool Security</h6>
-                    <span>$30.00</span>
-                  </div>
-                </a>
-                <a href="#" class="latest-product__item">
-                  <div class="latest-product__item__pic">
-                    <img src="assets/img/latest-product/lp-2.jpg" alt="" />
-                  </div>
-                  <div class="latest-product__item__text">
-                    <h6>Crab Pool Security</h6>
-                    <span>$30.00</span>
-                  </div>
-                </a>
-                <a href="#" class="latest-product__item">
-                  <div class="latest-product__item__pic">
-                    <img src="assets/img/latest-product/lp-3.jpg" alt="" />
-                  </div>
-                  <div class="latest-product__item__text">
-                    <h6>Crab Pool Security</h6>
-                    <span>$30.00</span>
-                  </div>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 col-md-6">
-          <div class="latest-product__text">
-            <h4>Review Products</h4>
-            <div class="latest-product__slider owl-carousel">
-              <div class="latest-prdouct__slider__item">
-                <a href="#" class="latest-product__item">
-                  <div class="latest-product__item__pic">
-                    <img src="assets/img/latest-product/lp-1.jpg" alt="" />
-                  </div>
-                  <div class="latest-product__item__text">
-                    <h6>Crab Pool Security</h6>
-                    <span>$30.00</span>
-                  </div>
-                </a>
-                <a href="#" class="latest-product__item">
-                  <div class="latest-product__item__pic">
-                    <img src="assets/img/latest-product/lp-2.jpg" alt="" />
-                  </div>
-                  <div class="latest-product__item__text">
-                    <h6>Crab Pool Security</h6>
-                    <span>$30.00</span>
-                  </div>
-                </a>
-                <a href="#" class="latest-product__item">
-                  <div class="latest-product__item__pic">
-                    <img src="assets/img/latest-product/lp-3.jpg" alt="" />
-                  </div>
-                  <div class="latest-product__item__text">
-                    <h6>Crab Pool Security</h6>
-                    <span>$30.00</span>
-                  </div>
-                </a>
-              </div>
-              <div class="latest-prdouct__slider__item">
-                <a href="#" class="latest-product__item">
-                  <div class="latest-product__item__pic">
-                    <img src="assets/img/latest-product/lp-1.jpg" alt="" />
-                  </div>
-                  <div class="latest-product__item__text">
-                    <h6>Crab Pool Security</h6>
-                    <span>$30.00</span>
-                  </div>
-                </a>
-                <a href="#" class="latest-product__item">
-                  <div class="latest-product__item__pic">
-                    <img src="assets/img/latest-product/lp-2.jpg" alt="" />
-                  </div>
-                  <div class="latest-product__item__text">
-                    <h6>Crab Pool Security</h6>
-                    <span>$30.00</span>
-                  </div>
-                </a>
-                <a href="#" class="latest-product__item">
-                  <div class="latest-product__item__pic">
-                    <img src="assets/img/latest-product/lp-3.jpg" alt="" />
-                  </div>
-                  <div class="latest-product__item__text">
-                    <h6>Crab Pool Security</h6>
-                    <span>$30.00</span>
-                  </div>
-                </a>
-              </div>
-            </div>
+            </router-link>
           </div>
         </div>
       </div>
@@ -203,7 +37,60 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
-    sname: 'ProductListCarousel'
-}
+  name: "ProductListCarousel",
+  data() {
+    return {
+      lowestProducts: [],
+    };
+  },
+  methods: {
+    getLowestPriceProduct() {
+      this.lowestProducts = this.products.filter(
+        (product) => product.price < 100
+      );
+    },
+  },
+  computed: mapGetters("products", ["products"]),
+  created() {
+    this.getLowestPriceProduct();
+  },
+};
 </script>
+
+<style scoped>
+.container h4 {
+  text-align: center;
+  font-weight: bold;
+  margin-bottom: 50px;
+}
+.products__section {
+  display: contents;
+}
+.lowest__product__item {
+  margin-bottom: 20px;
+  overflow: hidden;
+  display: block;
+}
+.lowest__product__item__pic {
+  float: left;
+  margin-right: 26px;
+  width: 3rem;
+}
+.lowest__product__item__text {
+  overflow: hidden;
+  padding-top: 10px;
+}
+.lowest__product__item__text span {
+  color: black;
+}
+.lowest__product__item__text span:hover {
+  font-weight: bold;
+}
+.lowest__product__item__text p a:hover {
+  font-weight: bold;
+  color: darkblue;
+}
+</style>
